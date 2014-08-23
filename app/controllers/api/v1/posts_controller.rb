b3d9fe958544997b7cba27de85c41392f6be84e3
rockyj <rockyj@localhost.localdomain>
2014-08-23 21:39:27 +0200

    minor code cleanup.

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,42 +2,42 @@ module Api
   module V1
     class PostsController < ApplicationController
       def index
-        @posts = Post.where("user_id = ?", current_user.id)
-        authorize @posts
-        render json: @posts
+        posts = Post.where("user_id = ?", current_user.id)
+        authorize posts
+        render json: posts
       end
 
       def show
-        @post = Post.find(params[:id])
-        authorize @post
-        render json: @post
+        post = Post.find(params[:id])
+        authorize post
+        render json: post
       end
 
       def create
-        @post = Post.new(post_params)
-        @post.user = current_user
-        authorize @post
-        if @post.save
-          render json: @post, status: :created, location: api_v1_post_path(@post)
+        post = Post.new(post_params)
+        post.user = current_user
+        authorize post
+        if post.save
+          render json: post, status: :created, location: api_v1_post_path(post)
         else
-          render json: @post.errors, status: :unprocessable_entity
+          render json: post.errors, status: :unprocessable_entity
         end
       end
 
       def update
-        @post = Post.find(params[:id])
-        authorize @post
-        if @post.update(post_params)
-          render json: @post
+        post = Post.find(params[:id])
+        authorize post
+        if post.update(post_params)
+          render json: post
         else
-          render json: @post.errors, status: :unprocessable_entity
+          render json: post.errors, status: :unprocessable_entity
         end
       end
 
       def destroy
-        @post = Post.find(params[:id])
-        authorize @post
-        @post.destroy
+        post = Post.find(params[:id])
+        authorize post
+        post.destroy
         head :no_content
       end
 

763da9e6bffb9c879a61e74a04eebe42bdac8d04
rockyj <rockyj@localhost.localdomain>
2014-08-23 21:48:24 +0200

    minor change in docker image building.

--- staging.sh
+++ staging.sh
@@ -3,6 +3,6 @@
 IMAGE=rockyj/ebenezer
 CONTAINER=ebenezer
 
-sudo docker build --rm=true -t $IMAGE .
+sudo docker build --rm=true --no-cache=false -t $IMAGE .
 sudo docker rm $CONTAINER
 sudo docker run -tid -p 3000:3000 --name $CONTAINER $IMAGE
